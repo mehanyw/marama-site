9bbc1cafa2e3486a27698953bfb3e0d218686024
Waseem Mehany <seemsmacbookair@seems-macbook.lan>
2023-01-27 06:22:17 -0600

    Updated the styles

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+window.onscroll = () => {
+    // Change nav color when scrolled
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+        document.getElementById('nav-bar').setAttribute('scrolled', true);
+    else
+        document.getElementById('nav-bar').setAttribute('scrolled', false);
+}
