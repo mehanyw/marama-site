d2169578da5f37dba38a3fc5866106378a7c2b77
Waseem Mehany <mehanyw@gmail.com>
2023-02-15 13:05:12 -0600

    Updated mobile styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,5 +3,22 @@ window.onscroll = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
         document.getElementById('nav-bar').setAttribute('scrolled', true);
     else
-        document.getElementById('nav-bar').setAttribute('scrolled', false);
+        document.getElementById('nav-bar').setAttribute('scrolled', false);    
+}
+
+function openNav(navButton)
+{
+    let navLinks = document.getElementById('nav-bar__mobile__links');
+    let navWrapper = document.getElementById('nav-bar');
+    if (navLinks)
+    {
+        navWrapper.hasAttribute('open') ? navWrapper.toggleAttribute('open') : navWrapper.setAttribute('open', true);
+        navButton.hasAttribute('open') ? navButton.toggleAttribute('open') : navButton.setAttribute('open', true);
+        navLinks.hasAttribute('open') ? navLinks.toggleAttribute('open') : navLinks.setAttribute('open', true);
+        document.body.hasAttribute('fixed') ? document.body.toggleAttribute('fixed') : document.body.setAttribute('fixed', true);
+
+        
+        // navLinks.setAttribute('open', true);
+        // document.body.setAttribute('fixed', true);
+    }
 }
