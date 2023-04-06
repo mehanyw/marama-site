5198c322295ac8d1975892b06923cee8339268a5
Waseem Mehany <mehanyw@gmail.com>
2023-04-06 14:25:36 -0500

    Added circle for video

--- assets/scripts/videoMoonScroller.js
+++ assets/scripts/videoMoonScroller.js
@@ -2,10 +2,10 @@
  * A Custom video scrollbar
  */
 
-let heroVideo               = document.getElementById('hero__video');
-let moonVideoScrollbar      = document.getElementById('moon__video__scrollbar');
-let moonVideoScrollbarTrack = document.getElementById('moon__video__scrollbar__track');
-
+let heroVideo                = document.getElementById('hero__video');
+let moonVideoScrollbar       = document.getElementById('moon__video__scrollbar');
+let moonVideoScrollbarTrack  = document.getElementById('moon__video__scrollbar__track');
+let moonVideoScrollbarCircle = document.getElementById('moon__video__scrollbar__circle');
 
 window.onload   = () => updateMoonVideoScrollbarPosition();
 window.onresize = () => updateMoonVideoScrollbarPosition();
@@ -16,18 +16,8 @@ function updateMoonVideoScrollbarPosition()
 	moonVideoScrollbar.setAttribute('height', `${heroVideo.clientHeight}`);
 
 	let trackPath = `M0,${heroVideo.clientHeight * 0.75} C${heroVideo.clientWidth * 0.25},-0 ${heroVideo.clientWidth * 0.75},-0 ${heroVideo.clientWidth},${heroVideo.clientHeight * 0.75}`;
-	moonVideoScrollbarTrack.setAttribute('d', trackPath);	
+	moonVideoScrollbarTrack.setAttribute('d', trackPath);
 }
-// heroVideo.currentTime = 5;
 
-
-// Playing event
-heroVideo.addEventListener("playing", updateScroller());
-function updateScroller()
-{
-	console.log(moonVideoScrollbar);
-	console.log(heroVideo.currentTime);	
-}
-// heroVideo.onplay = () => {
-// 	console.log(heroVideo.currentTime);	
-// };
+heroVideo.onwaiting = () => moonVideoScrollbar.pauseAnimations();
+heroVideo.ontimeupdate = () => moonVideoScrollbar.unpauseAnimations();
