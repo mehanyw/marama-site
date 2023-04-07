7e122681eb7d32c0a6a3bd13ada6ba63c8f09a25
Waseem Mehany <mehanyw@gmail.com>
2023-04-07 16:56:51 -0500

    Update js for moon video mobile

--- assets/scripts/videoMoonScroller.js
+++ assets/scripts/videoMoonScroller.js
@@ -12,10 +12,16 @@ window.onresize = () => updateMoonVideoScrollbarPosition();
 
 function updateMoonVideoScrollbarPosition()
 {
+	console.log(window.innerWidth);
 	moonVideoScrollbar.setAttribute('width', `${heroVideo.clientWidth}`);
 	moonVideoScrollbar.setAttribute('height', `${heroVideo.clientHeight}`);
 
-	let trackPath = `M0,${heroVideo.clientHeight * 0.75} C${heroVideo.clientWidth * 0.25},-0 ${heroVideo.clientWidth * 0.75},-0 ${heroVideo.clientWidth},${heroVideo.clientHeight * 0.75}`;
+	let trackPath;
+	if (window.innerWidth > 768)
+		trackPath = `M0,${heroVideo.clientHeight * 0.75} C${heroVideo.clientWidth * 0.25},-0 ${heroVideo.clientWidth * 0.75},-0 ${heroVideo.clientWidth},${heroVideo.clientHeight * 0.75}`;
+	else
+		trackPath = `M-${heroVideo.clientWidth * 0.25},${heroVideo.clientHeight * 0.9} C${heroVideo.clientWidth * 0.0},-0 ${heroVideo.clientWidth * 1},-0 ${heroVideo.clientWidth * 1.25},${heroVideo.clientHeight * 0.9}`;
+
 	moonVideoScrollbarTrack.setAttribute('d', trackPath);
 }
 
