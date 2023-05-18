194b75c93d45431d818005ced92bd75afff9d783
Waseem Mehany <mehanyw@gmail.com>
2023-05-18 12:43:25 -0500

    Added php mail for contact, google analytics, & 404 page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ window.onscroll = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
         document.getElementById('nav-bar').setAttribute('scrolled', true);
     else
-        document.getElementById('nav-bar').setAttribute('scrolled', false);    
+        document.getElementById('nav-bar').setAttribute('scrolled', false);   
 }
 
 function openNav(navButton)
---
+++ contactMailer.php
@@ -0,0 +1,51 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+
+// Definitions
+$USER_NAME = 'info@livemarama.com';
+$PASSWORD = 'QC*g@J7p9Th2rFay';
+$SMTP_HOST = 'ssl://smtp.titan.email';
+$SMTP_PORT = '465';
+
+// SMTP setup
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = $SMTP_HOST;
+$mail->Port = $SMTP_PORT;
+$mail->SMTPDebug = 0;
+$mail->SMTPAuth = true;
+$mail->Username = $USER_NAME;
+$mail->Password = $PASSWORD;
+
+// Email headers
+$mail->setFrom($USER_NAME, 'Live Marama');
+$mail->addAddress('mehanyw@gmail.com', 'Waseem Mehany');
+$mail->addAddress('danielpaley@gmail.com', 'Daniel Paley');
+$mail->Subject = 'Contact Form';
+
+// Email body
+$mail->isHTML(false);
+$referredBy = "-";
+if (array_key_exists('referred-by', $_POST)) {
+    $referredBy = $_POST["referred-by"];
+}
+$mail->Body = <<<EOT
+	First name: {$_POST["first-name"]}
+	Last name: {$_POST["last-name"]}
+	Email: {$_POST["email"]}
+	Phone number: {$_POST["phone"]}
+	Zip code: {$_POST["zip"]}
+
+	How did you hear about us?: {$referredBy}
+
+	Are you a broker?: {$_POST["broker"]}
+	Are you working with a broker?: {$_POST["with-broker"]}
+EOT;
+
+if (!$mail->send()) {
+	echo '<script>alert("Welcome to Geeks for Geeks")</script>';
+} else {
+	header("Location:https://www.livemarama.com/pages/contactSuccess.html");
+}
+?>
